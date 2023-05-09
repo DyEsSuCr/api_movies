b1ba7f83b9415412f1e27c99fa31f8b5bf12de45
DylanSuarez <2000sajo@gmail.com>
2023-05-09 18:43:02 -0500

    host

--- config.js
+++ config.js
@@ -6,10 +6,12 @@ const USER = process.env.USER
 const PWD = process.env.PWD
 const DB = process.env.DB
 const PORT = process.env.PORT
+const HOST = process.env.HOST
 
 export const env = {
   USER,
   PWD,
   DB,
-  PORT
+  PORT,
+  HOST
 }
--- database/db.js
+++ database/db.js
@@ -3,5 +3,5 @@ import { env } from '../config.js'
 
 export const conectionSequelize = new Sequelize(env.DB, env.USER, env.PWD, {
   dialect: 'mysql',
-  database: env.DB
+  host: env.HOST
 })
